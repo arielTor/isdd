2e4f700b77caf16da39b19cf3235104bb32189c3
Ariel Torjamn <arieltorjamn@Ariels-MacBook-Pro.local>
2026-05-07 01:15:49 +0300

    Allow all hosts in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({mode}) => {
     },
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
-      allowedHosts: ['i-sdd-production.up.railway.app'],
+      allowedHosts: 'all',
     },
   };
 });
